2de3e918843652f758d3c7d9fdb968198e09d79d
Tom Burnett <t_burnett@outlook.coml>
2017-01-30 11:16:07 +0000

    Initial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Nav from 'Nav';
+
+const Main = () => {
+    return (
+        <div>
+            <Nav />
+            <div className="row">
+                <div>
+                    <p>Main.jsx</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+module.exports = Main;
